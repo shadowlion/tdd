2fcaa95eaeb8a504270f4b8ba644793fa9dfce53
Justin Chiou <chiou.kai@gmail.com>
2019-02-13 15:38:25 -0500

    add firebase integration for Storage features

--- src/main.js
+++ src/main.js
@@ -3,7 +3,18 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import VueYouTubeEmbed from 'vue-youtube-embed'
+import firebase from 'firebase/app'
 
+const config = {
+  apiKey: process.env.VUE_APP_apikey,
+  authDomain: process.env.VUE_APP_authDomain,
+  databaseURL: process.env.VUE_APP_databaseURL,
+  projectId: process.env.VUE_APP_projectId,
+  storageBucket: process.env.VUE_APP_storageBucket,
+  messagingSenderId: process.env.VUE_APP_messagingSenderId
+}
+
+firebase.initializeApp(config)
 Vue.use(VueYouTubeEmbed)
 Vue.config.productionTip = false
 
